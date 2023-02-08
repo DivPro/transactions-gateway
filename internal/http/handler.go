ae5f6f2ff84f704f7ee5b4610eaa2973fa04eb87
Aleksey Shatunov <a.shatunov@qvant.ru>
2023-02-08 15:22:11 +0300

    external id support

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"encoding/json"
+	"github.com/google/uuid"
 	"net/http"
 
 	"github.com/Shopify/sarama"
@@ -20,11 +21,11 @@ func (h Handler) DepositCreate(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	var request api.Deposit
-
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	request.ID = uuid.NewString()
 
 	// TODO from auth
 	request.UserID = "0a68a321-de8e-48ef-83b3-c256e087d310"
@@ -37,6 +38,7 @@ func (h Handler) DepositCreate(w http.ResponseWriter, r *http.Request) {
 
 	_, _, err = h.producer.SendMessage(&sarama.ProducerMessage{
 		Topic: "deposits",
+		Key:   sarama.ByteEncoder(request.UserID),
 		Value: sarama.ByteEncoder(data),
 	})
 	if err != nil {
@@ -44,12 +46,10 @@ func (h Handler) DepositCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	if err := json.NewEncoder(w).Encode(request.ID); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	w.WriteHeader(http.StatusCreated)
 }
 
 func (h Handler) TransactionCreate(w http.ResponseWriter, r *http.Request) {
@@ -61,6 +61,7 @@ func (h Handler) TransactionCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	request.TargetID = request.UserID
+	request.ID = uuid.NewString()
 
 	// TODO from auth
 	request.UserID = "adeb0c95-a333-4049-9c27-beb34ec3cd32"
@@ -73,6 +74,7 @@ func (h Handler) TransactionCreate(w http.ResponseWriter, r *http.Request) {
 
 	_, _, err = h.producer.SendMessage(&sarama.ProducerMessage{
 		Topic: "transactions",
+		Key:   sarama.ByteEncoder(request.UserID),
 		Value: sarama.ByteEncoder(data),
 	})
 	if err != nil {
@@ -80,12 +82,10 @@ func (h Handler) TransactionCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	if err := json.NewEncoder(w).Encode(request.ID); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
-	w.WriteHeader(http.StatusCreated)
 }
 
 func (h Handler) TransactionList(w http.ResponseWriter, req *http.Request) {
